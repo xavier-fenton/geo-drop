36d20bec4afcc7f61ff408bf15ea5886f0faba52
Xavier <xavierfenton99@gmail.com>
2022-09-24 15:20:19 +1200

    add new seed locations to test the auth0

--- server/db/seeds/messages.js
+++ server/db/seeds/messages.js
@@ -7,31 +7,38 @@ exports.seed = function (knex) {
       return knex('messages').insert([
         {
           id: 1,
-          lat: -36.8645,
-          long: 174.7766,
+          lat: -36.8682574,
+          long: 174.7656955,
           name: 'taylor',
           msg: 'hello from couch',
         },
         {
           id: 2,
-          lat: -36.8647,
-          long: 174.77652,
+          lat: -36.8682574,
+          long: 174.7656955,
           name: 'frey',
           msg: 'Hello from pantry',
         },
         {
           id: 3,
-          lat: -36.8645,
-          long: 174.7765,
+          lat: -36.8682574,
+          long: 174.7656955,
           name: 'xavier',
           msg: 'Hello from yoga',
         },
         {
           id: 4,
-          lat: -36.88272252836678,
-          long: 174.7613017129278,
+          lat: -36.8682574,
+          long: 174.7656955,
           name: 'benson',
           msg: 'Hello from Bensons Home',
+        },
+        {
+          id: 5,
+          lat: 35.689487,
+          long: 174.7656955,
+          name: 'Somewhere in tokyo',
+          msg: 'Hello from TOKYO',
         },
       ])
     })
