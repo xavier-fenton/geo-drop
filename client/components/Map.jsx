6ef6eb4a2e5353b74694f8eecdce96d1f6343577
ahmad <eng.ahmed.anwar@gmail.com>
2022-09-26 12:08:21 +1300

    install and show leaflet

--- client/components/Map.jsx
+++ client/components/Map.jsx
@@ -1,18 +1,18 @@
 import React from 'react'
+import * as L from 'leaflet'
+
 import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet'
 
 export default function Map() {
+
   return (
-    <MapContainer center={[51.505, -0.09]} zoom={13} scrollWheelZoom={false}>
-      <TileLayer
-        attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-        url="https://api.mapbox.com/styles/v1/mapbox/streets-v11/tiles/{z}/{x}/{y}?access_token=pk.eyJ1IjoibWljaGFlbC1yIiwiYSI6ImNrZXM1Zm9iaDJiNmYycW1za2dobDZ4d3gifQ.a5mK2DxNqWhlzvoa8Zxb2Q"
-      />
-      <Marker position={[51.505, -0.09]}>
-        <Popup>
-          A pretty CSS3 popup. <br /> Easily customizable.
-        </Popup>
-      </Marker>
+    <MapContainer
+      className="h-full rounded-lg mb-6"
+      center={[-36.86667, 174.76667]}
+      zoom={11}
+      scrollWheelZoom={true}
+    >
+      <TileLayer url="https://api.mapbox.com/styles/v1/mapbox/streets-v11/tiles/{z}/{x}/{y}?access_token=<your token goes here>" />
     </MapContainer>
   )
 }
