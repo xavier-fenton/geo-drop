9b985ad185bea183d598f3693188e59b848d0b97
Xavier <xavierfenton99@gmail.com>
2022-09-25 14:31:15 +1300

    fix lint

--- client/components/Form.jsx
+++ client/components/Form.jsx
@@ -1,8 +1,6 @@
-import React, { useState, useEffect } from 'react'
+import React, { useState } from 'react'
 import { useSelector } from 'react-redux'
-import { addMessages, getMessages } from '../api'
-
-// import { addMessage } from '../apiClient'
+import { addMessages } from '../api'
 
 // ADD SLICE...
 
