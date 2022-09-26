f664087fe6d1be022cf5fc5da04640885466567f
Taylor Wagstaff <taylorwagstaff@gmail.com>
2022-09-27 11:15:07 +1300

    bio page in profile

--- client/components/Map.jsx
+++ client/components/Map.jsx
@@ -1,13 +1,9 @@
 import React, { useState, useEffect } from 'react'
 
-// import { getIsAuthenticated } from '../auth0-utils'
-// import { useAuth0 } from '@auth0/auth0-react'
-// import { getLoginFn, getLogoutFn, getRegisterFn } from '../auth0-utils'
-// import { IfAuthenticated, IfNotAuthenticated } from './Authenticated'
-
 import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet'
 import { useAuth0 } from '@auth0/auth0-react'
 import { getMessagesById } from '../api'
+import Bio from './Bio'
 
 //map
 export default function Map() {
@@ -28,20 +24,22 @@ export default function Map() {
   }, [isAuthenticated])
 
   return (
-    <MapContainer
-      className="h-full rounded-lg mb-6"
-      center={{ lat: -36.8682574, lng: 174.7656955 }}
-      zoom={11}
-      scrollWheelZoom={true}
-    >
-      {messages.map(({ lat, long, msg }, index) => (
-        <Marker position={[lat, long]} key={index}>
-          <Popup>
-            {msg}
-          </Popup>
-        </Marker>
-      ))}
-      <TileLayer url="https://api.mapbox.com/styles/v1/mapbox/streets-v11/tiles/{z}/{x}/{y}?access_token=pk.eyJ1IjoibWljaGFlbC1yIiwiYSI6ImNrZXM1Zm9iaDJiNmYycW1za2dobDZ4d3gifQ.a5mK2DxNqWhlzvoa8Zxb2Q" />
-    </MapContainer>
+    <>
+      <Bio />
+
+      <MapContainer
+        className="h-full rounded-lg mb-6"
+        center={{ lat: -36.8682574, lng: 174.7656955 }}
+        zoom={12}
+        scrollWheelZoom={true}
+      >
+        {messages.map(({ lat, long, msg }, index) => (
+          <Marker position={[lat, long]} key={index}>
+            <Popup>{msg}</Popup>
+          </Marker>
+        ))}
+        <TileLayer url="https://api.mapbox.com/styles/v1/mapbox/streets-v11/tiles/{z}/{x}/{y}?access_token=pk.eyJ1IjoibWljaGFlbC1yIiwiYSI6ImNrZXM1Zm9iaDJiNmYycW1za2dobDZ4d3gifQ.a5mK2DxNqWhlzvoa8Zxb2Q" />
+      </MapContainer>
+    </>
   )
 }
