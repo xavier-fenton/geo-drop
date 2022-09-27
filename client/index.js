716cc56810fd58c2decd4ba35bb429600f23cf91
ahmad <eng.ahmed.anwar@gmail.com>
2022-09-28 10:47:15 +1300

    remove unused import

--- client/index.js
+++ client/index.js
@@ -5,7 +5,6 @@ import { BrowserRouter as Router } from 'react-router-dom'
 import store from './store'
 
 // eslint-disable-next-line import/no-unresolved
-import config from './auth_config.json'
 
 import App from './components/App'
 import { Auth0Provider } from '@auth0/auth0-react'
