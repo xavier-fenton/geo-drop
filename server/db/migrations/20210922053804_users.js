92a7cde3b2267dab00f3ae2f2db2acef28ca4454
ahmad <eng.ahmed.anwar@gmail.com>
2022-09-28 17:53:26 +1300

    make id to be primary

--- server/db/migrations/20210922053804_users.js
+++ server/db/migrations/20210922053804_users.js
@@ -1,6 +1,6 @@
 exports.up = function (knex) {
   return knex.schema.createTable('users', (table) => {
-    table.increments('id')
+    table.increments('id').primary()
     table.string('auth0_id')
     table.string('name')
     table.string('email')
